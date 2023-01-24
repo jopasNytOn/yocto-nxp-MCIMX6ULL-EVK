685edd38f6214db9d761b3da347da7806e494885
jopasNytOn <jopasnyton@saunalahti.fi>
2023-01-24 21:26:14 +0200

    Remove GPLv3 licensed from image

--- build_yocto.sh
+++ build_yocto.sh
@@ -3,11 +3,17 @@ sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib buil
 if [ -d "bin/repo" ]; then echo "repo already downloaded"; else mkdir -p bin; curl https://storage.googleapis.com/git-repo-downloads/repo > bin/repo; chmod a+x bin/repo; fi
 export PATH=$PATH:$PWD/bin
 
+git -C sources/meta-gplv2/ checkout zeus
+
 mkdir -p imx-yocto-bsp && cd imx-yocto-bsp
 repo init -u https://source.codeaurora.org/external/imx/imx-manifest -b imx-linux-zeus -m imx-5.4.70-2.3.0.xml
 repo sync
 
 DISTRO=fsl-imx-fb MACHINE=imx6ull14x14evk source sources/meta-imx/tools/imx-setup-release.sh -b yocto-nxp-MCIMX6ULL-EVK
+
+cp ../../sources/bblayers.conf conf/
+echo 'INCOMPATIBLE_LICENSE = "GPLv3 GPLv3+ LGPLv3 LGPLv3+"' >> conf/local.conf
+
 bitbake core-image-base
 
 cd ../.. && if [ -d "artifacts" ]; then rm -rf artifacts; fi && mkdir -p artifacts
